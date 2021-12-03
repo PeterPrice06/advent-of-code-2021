5a37530d8708731f03f5c3ad7ec2235e79efda03
Peter Price <linebeck06@gmail.com>
2021-12-02 21:32:13 -0500

    Complete second half of day 2 solution

--- day-2/solution.py
+++ day-2/solution.py
@@ -7,6 +7,24 @@ class Solution:
         self.UP_KEY = 'up '
         pass
 
+    def calculate_position(self, moves: List[str]) -> Tuple[int, int]:
+        horz = 0
+        depth = 0
+        aim = 0
+        for i, move in enumerate(moves):
+            if move.startswith(self.FORWARD_KEY):
+                distance = int(move[len(self.FORWARD_KEY):])
+                horz += distance
+                depth += aim * distance
+            elif move.startswith(self.DOWN_KEY):
+                distance = int(move[len(self.DOWN_KEY):])
+                aim += distance
+            elif move.startswith(self.UP_KEY):
+                distance = int(move[len(self.UP_KEY):])
+                aim -= distance
+ 
+        return horz, depth
+
     def calculate_naive_movement_position(self, moves: List[str]) -> Tuple[int, int]:
         horz = 0
         depth = 0
--- day-2/test.py
+++ day-2/test.py
@@ -7,17 +7,29 @@ class TestSolution(unittest.TestCase):
         self.solution = solution.Solution()
         pass
 
-    def test_simple_calculate_position(self) -> None:
+    def test_simple_calculate_naive_movement_position(self) -> None:
         moves = file_read_helper('simple_input.txt')
         horz, depth = self.solution.calculate_naive_movement_position(moves)
         self.assertEqual(horz, 15)
         self.assertEqual(depth, 10)
 
-    def test_long_calculate_position(self) -> None:
+    def test_long_calculate_naive_movement_position(self) -> None:
         moves = file_read_helper('long_input.txt')
         horz, depth = self.solution.calculate_naive_movement_position(moves)
         self.assertEqual(horz, 1998)
         self.assertEqual(depth, 741)
+
+    def test_simple_calculate_position(self) -> None:
+        moves = file_read_helper('simple_input.txt')
+        horz, depth = self.solution.calculate_position(moves)
+        self.assertEqual(horz, 15)
+        self.assertEqual(depth, 60)
+
+    def test_long_calculate_position(self) -> None:
+        moves = file_read_helper('long_input.txt')
+        horz, depth = self.solution.calculate_position(moves)
+        self.assertEqual(horz, 1998)
+        self.assertEqual(depth, 642047)
 
 def file_read_helper(filename: str) -> List[str]:
     lines = []
