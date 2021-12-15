c8c138a11451a7b84a0fbfa095fc2ff640ae25bc
Peter Price <linebeck06@gmail.com>
2021-12-14 23:28:04 -0500

    Day-14 off by one issues

--- day-14/polymer.py
+++ day-14/polymer.py
@@ -4,45 +4,50 @@ class PolymerExtruder():
     def __init__(self, line_strs: List[str]) -> None:
         self.polymer_template = line_strs[0]
         self.insertion_rules = []
-        self.polymer = self.polymer_template
+        self.polymer_pairs = {}
+        for i in range(len(self.polymer_template) - 1):
+            pair = self.polymer_template[i:i + 2]
+            if pair not in self.polymer_pairs:
+                self.polymer_pairs[pair] = 1
+            else:
+                self.polymer_pairs[pair] += 1
 
         for line_str in line_strs[2:]:
             split_line = line_str.split(' -> ')
             self.insertion_rules.append((split_line[0], split_line[1]))
     
     def extrude(self) -> None:
-        new_polymer = ''
-        for i in range(len(self.polymer) - 1):
-            new_polymer += self.polymer[i]
-            for rule in self.insertion_rules:
-                polymer_segment = self.polymer[i:i + len(rule[0])]
-                if polymer_segment == rule[0]:
-                    new_polymer += rule[1]
-        new_polymer += polymer_segment[1]
-        self.polymer = new_polymer
-
+        new_polymer_pairs = {}
+        for rule in self.insertion_rules:
+            if rule[0] in self.polymer_pairs:
+                pair_count = self.polymer_pairs[rule[0]]
+                new_pair_first_half = rule[0][0] + rule[1]
+                if new_pair_first_half not in new_polymer_pairs:
+                    new_polymer_pairs[new_pair_first_half] = pair_count
+                else:
+                    new_polymer_pairs[new_pair_first_half] += pair_count
+                new_pair_second_half = rule[1] + rule[0][1]
+                if new_pair_second_half not in new_polymer_pairs:
+                    new_polymer_pairs[new_pair_second_half] = pair_count
+                else:
+                    new_polymer_pairs[new_pair_second_half] += pair_count
+        self.polymer_pairs = new_polymer_pairs.copy()
+            
     def extrude_multiple(self, count: int) -> None:
         for i in range(count):
-            print(f'Step {i}: {self.polymer_score()}')
+            print(f'Step {i}: {self.polymer_score()}\t| {self.polymer_pairs}')
             self.extrude()
 
-
     def polymer_score(self) -> int:
-        return self.polymer_max_score() - self.polymer_min_score()
-
-    def polymer_max_score(self) -> int:
         counts = self.polymer_char_counts()
-        return max(counts.values())
+        return max(counts.values()) - min(counts.values())
 
-    def polymer_min_score(self) -> int:
-        counts = self.polymer_char_counts()
-        return min(counts.values())
-        
     def polymer_char_counts(self) -> Dict[str, int]:
         counts = {}
-        for char in self.polymer:
+        for pair in self.polymer_pairs:
+            char = pair[0]
             if char not in counts:
-                counts[char] = 1
+                counts[char] = self.polymer_pairs[pair]
             else:
-                counts[char] += 1
+                counts[char] += self.polymer_pairs[pair]
         return counts
--- day-14/test.py
+++ day-14/test.py
@@ -6,24 +6,10 @@ class TestCaveNavigator(unittest.TestCase):
     def setUp(self) -> None:
         pass
 
-    def test_extrude_polymer_once(self) -> None:
-        lines = file_read_helper('day-14/sample_input.txt')
-        polymer_extruder = PolymerExtruder(lines)
-        polymer_extruder.extrude()
-        self.assertEqual(polymer_extruder.polymer, 'NCNBCHB')
-
-    def test_extrude_polymer_four_times(self) -> None:
-        lines = file_read_helper('day-14/sample_input.txt')
-        polymer_extruder = PolymerExtruder(lines)
-        polymer_extruder.extrude_multiple(4)
-        self.assertEqual(polymer_extruder.polymer, 'NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB')
-
     def test_extrude_polymer_ten_times(self) -> None:
         lines = file_read_helper('day-14/sample_input.txt')
         polymer_extruder = PolymerExtruder(lines)
         polymer_extruder.extrude_multiple(10)
-        self.assertEqual(polymer_extruder.polymer_max_score(), 1749)
-        self.assertEqual(polymer_extruder.polymer_min_score(), 161)
         self.assertEqual(polymer_extruder.polymer_score(), 1588)
 
     def test_extrude_puzzle_polymer_ten_times(self) -> None:
@@ -32,17 +18,17 @@ class TestCaveNavigator(unittest.TestCase):
         polymer_extruder.extrude_multiple(10)
         self.assertEqual(polymer_extruder.polymer_score(), 3284)
 
-    #def test_extrude_sample_polymer_fourty_times(self) -> None:
-    #    lines = file_read_helper('day-14/sample_input.txt')
-    #    polymer_extruder = PolymerExtruder(lines)
-    #    polymer_extruder.extrude_multiple(40)
-    #    self.assertEqual(polymer_extruder.polymer_score(), 2188189693529)
+    def test_extrude_sample_polymer_fourty_times(self) -> None:
+        lines = file_read_helper('day-14/sample_input.txt')
+        polymer_extruder = PolymerExtruder(lines)
+        polymer_extruder.extrude_multiple(40)
+        self.assertEqual(polymer_extruder.polymer_score(), 2188189693529)
 
-    #def test_extrude_puzzle_polymer_fourty_times(self) -> None:
-    #    lines = file_read_helper('day-14/puzzle_input.txt')
-    #    polymer_extruder = PolymerExtruder(lines)
-    #    polymer_extruder.extrude_multiple(40)
-    #    self.assertEqual(polymer_extruder.polymer_score(), -1)
+    def test_extrude_puzzle_polymer_fourty_times(self) -> None:
+        lines = file_read_helper('day-14/puzzle_input.txt')
+        polymer_extruder = PolymerExtruder(lines)
+        polymer_extruder.extrude_multiple(40)
+        self.assertEqual(polymer_extruder.polymer_score(), 4302675529689)# 4302675529690 to high
 
 
 
